b7e78c532578c74de8253eb6cfa9de7fa711125d
soltan1201 <solkan1201@gmail.com>
2026-02-12 19:40:01 -0300

    subindo scripts de join of tifs

--- src/system_predict/stitch_mosaics.sh
+++ src/system_predict/stitch_mosaics.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # 1. Ajuste os caminhos (Sem a barra final para evitar confusão)
-INPUT_FOLDER="/home/superusuario/db_images/predAlerts/PATCHS_S2_Outubro_CAAT"
-FINAL_OUTPUT_DIR="/home/superusuario/db_images/rasters_alerts/PATCHS_S2_Outubro_CAAT"
-OUTPUT_FILE="$FINAL_OUTPUT_DIR/PATCHS_S2_Outubro_CAAT.tif"
+INPUT_FOLDER="/home/superusuario/db_images/predAlerts/PATCHS_S2_Novembro_CAAT"
+FINAL_OUTPUT_DIR="/home/superusuario/db_images/rasters_alerts/PATCHS_S2_Novembro_CAAT"
+OUTPUT_FILE="$FINAL_OUTPUT_DIR/PATCHS_S2_Novembro_CAAT.tif"
 
 # 2. Criar a pasta de saída
 mkdir -p "$FINAL_OUTPUT_DIR"
